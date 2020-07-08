630db3a4fb7a37e2c0bdf1570aada1ad91e242cc
michelvu2016 <anhsaodem@yahoo.com>
2020-07-07 22:50:12 -0700

    Initial commit

---
+++ NameTag.java
@@ -0,0 +1,16 @@
+class NameTag {
+	private String fullName;
+	private String employeeNumber;
+	private String deptNumber;
+
+	public NameTag() {}
+
+	public NameTag(String fullName, String employeeNumber, String deptNamber) {
+		this.fullName = fullName;
+		this.employeeNumber = employeeNumber;
+		this.deptNumber = deptNumber;
+	}
+
+
+
+}
